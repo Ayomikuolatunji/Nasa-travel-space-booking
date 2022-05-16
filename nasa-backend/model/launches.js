48c14bfdbf654ee3980326b7593ae0f443284924
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-16 23:01:35 +0100

    add  mission

--- nasa-backend/model/launches.js
+++ nasa-backend/model/launches.js
@@ -1,12 +1,14 @@
 const launches= new Map()
 
+let launchFlightNumber=100
+
 const launch={
-    flightNumber:100,
+    flightNumber:launchFlightNumber,
     mission:"kepler Exploration X",
     rocket:"Explorer IS1",
     launchDate:new Date("December 27, 2030"),
     desination:"Kepler-442 b",
-    customers:["MTP", "NASA"],
+    customer:["MTP", "NASA"],
     upcoming:true,
     success:true
 }
@@ -15,6 +17,13 @@ launches.set(launch.flightNumber, launch)
 
 const getLaunches=()=>{
     return Array.from(launches.values())
+}
+
+const addLaunch=(launch)=>{
+    launchFlightNumber++;
+    launches.set(launch.flightNumber, Object.assign(launch,{
+      flightNumber:launchFlightNumber
+    }))
 }
 
 module.exports={
