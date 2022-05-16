1fa1ca4ba0c32f294153a6fd31620dba2eb310c4
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-16 16:32:20 +0100

    serve frontend to backend server

--- nasa-backend/app.js
+++ nasa-backend/app.js
@@ -23,13 +23,12 @@ const corsOptions = {
 // app.use(cors())
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "..", "public")))
-app.use(express.static("public"))
-
-
-
+app.use(express.static(path.join(__dirname, "public")))
 app.use(planetsRoute)
 
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
  const load=async()=>{
   await loadData()
