686398da862a59a77e6fc73cab6b6c7f218a2a89
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-16 23:42:26 +0100

    add  mission

--- nasa-backend/controller/getLaunches.js
+++ nasa-backend/controller/getLaunches.js
@@ -8,6 +8,12 @@ const getAllLaunches=(req,res,next)=>{
 
 const postLaunch=(req,res,next)=>{
     const launch=req.body
+
+    if(!launch.mission || !launch.launchDate || !launch.rocket){
+        res.status(400).json({
+            message:"bad request"
+        })
+    }
     launch.launchDate=new Date(launch.launchDate)
     addLaunch(launch)
 
