fdd3bf86ca995cc2866feaf8527bef3ac376e91b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-17 13:57:38 +0100

    testing continue

--- nasa-backend/routes/launches.test.js
+++ nasa-backend/routes/launches.test.js
@@ -4,10 +4,16 @@ describe("GET LAUNCHES",()=>{
     test('should test launch get api', () => { 
 
         })
+})
+
+describe("POST LAUNCH",()=>{
     test('should post request to be sucess of 201', () => { 
 
     })
-    test('should error when post a launch with statusCode 400', () => { 
+    test('should error when post a launch with statusCode 400 with', () => { 
+        
+    })
+    test('should error when post a launch with statusCode 400 with', () => { 
         
     })
 })
