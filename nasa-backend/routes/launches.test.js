0513796f7982d6276ed80710e7b4367a62337a4c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-23 21:49:31 +0100

    clear

--- nasa-backend/routes/launches.test.js
+++ nasa-backend/routes/launches.test.js
@@ -15,48 +15,21 @@ describe("POST LAUNCH",()=>{
         mission:"kepler Exploration X",
         rocket:"Explorer IS1",
         destination:"Kepler-442 b",
-        launchDate:"December 27, 2030"
     }
     const launchData={
         mission:"kepler Exploration X",
         rocket:"Explorer IS1",
         destination:"Kepler-442 b",
-        launchDate:"December 27, 2030"
     }
 
-    // test('should test post launch api with statuscode 200', async() => { 
-    //     const res=await requests(app)
-    //     .post("/launches")
-    //     .set('Accept', 'application/json')
-    //     .send(completLaunchData)
-    //     const launchRes=new Date(completLaunchData.launchDate).valueOf()
-    //     const responseDate=new Date(res.body.launchDate).valueOf()
-    //     expect(responseDate).toBe(launchRes)
-    //     expect(res.body).toMatchObject(launchData)    
+    test('should test post launch api with statuscode 200', async() => { 
+        const res=await requests(app)
+        .post("/launches")
+        .set('Accept', 'application/json')
+        .send(completLaunchData)
+        expect(completLaunchData).toMatchObject(launchData)    
     //  })
-    const houseForSale = {
-        bath: true,
-        bedrooms: 4,
-        kitchen: {
-          amenities: ['oven', 'stove', 'washer'],
-          area: 20,
-          wallColor: 'white',
-        },
-      };
-      const desiredHouse = {
-        bath: true,
-        kitchen: {
-          amenities: ['oven', 'stove', 'washer'],
-          wallColor: expect.stringMatching(/white|yellow/),
-        },
-      };
-    it('responds with json',async()=>{
-        const res= requests(app)
-        .post("/users")
-        .set("Accept", "application.json")
-        .send(houseForSale)
-         expect(houseForSale).toMatchObject(desiredHouse);
-    });
 
 })
 
+})
