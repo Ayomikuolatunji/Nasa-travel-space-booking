9db9b8b5b3f182de80acb9af9e2fac0dac801067
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-24 12:21:11 +0100

    planets populated to the database

--- nasa-backend/controller/getAllPlanets.js
+++ nasa-backend/controller/getAllPlanets.js
@@ -1,9 +1,9 @@
-const {planets}=require("../model/planets")
+const planets = require("../model/planets")
 
 
 const getAllPlanets=async(req,res,enxt)=>{
-
-   res.status(200).json(planets)    
+   const allPlanets=await planets.find({})
+   res.status(200).json(allPlanets)    
 }
 
 
