8a0b8e87039f6c17292fae7894dbe119bbd2119b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-25 00:05:54 +0100

    solving mongodb auto increment problem

--- nasa-backend/controller/createLaunch.js
+++ nasa-backend/controller/createLaunch.js
@@ -2,26 +2,30 @@ const lauches=require("../model/launches")
 const planets = require("../model/planets")
 
 const postLaunch=async(req,res,next)=>{
-    const launch=req.body
-    
-    if(!launch.mission || !launch.launchDate || !launch.rocket){
-        res.status(400).json({
-            error:"bad request"
-        })
-    }
-    const findOne=await planets.findOne({
-        kepler_name:launch.destination
-    })
-    if(!findOne){
-        throw new Error("No matching planets was found")
-    }
-     let newLaunchNum=99
-     if(launch){
-       newLaunchNum+1
-     }
+    let defaultLaunchNum=100
      try {
+        const launch=req.body
+    
+        if(!launch.mission || !launch.launchDate || !launch.rocket){
+            res.status(400).json({
+                error:"bad request"
+            })
+        }
+        const findOne=await planets.findOne({
+            kepler_name:launch.destination
+        })
+        if(!findOne){
+            throw new Error("No matching planets was found")
+        }
+        const flightNumIncrement=await lauches
+        .findOne()
+        .sort("-flightNumber")
+        if(flightNumIncrement){
+            defaultLaunchNum=flightNumIncrement.flightNumber+1
+        }
+
         const newLaunch=await lauches.create({
-            flightNumber:newLaunchNum,
+            flightNumber:defaultLaunchNum,
             mission:launch.mission,
             rocket:launch.rocket,
             launchDate:launch.launchDate,
