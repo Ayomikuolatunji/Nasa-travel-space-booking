63bcb7046f9a1773acb780d6595bf4bd0a07f532
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-25 01:13:11 +0100

    clear

--- nasa-backend/controller/abortLaunch.js
+++ nasa-backend/controller/abortLaunch.js
@@ -5,7 +5,6 @@ const launches = require("../model/launches")
 
 const abortLaunch=async(req,res)=>{
  const flightNumber=+req.params.id
- const launchId=await launches.findOne({flightNumber:flightNumber})
   const launch=await launches.updateOne({flightNumber:flightNumber},{
     success:false,
     upcoming:false
